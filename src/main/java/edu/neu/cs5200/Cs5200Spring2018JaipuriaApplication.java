088b104ff8d4c610c99515130d3f6e7bedbda636
Pratik <jaipuria.p@husky.neu.edu>
2018-01-17 23:09:30 -0500

    Extended SpringBootServeletInitializer

--- src/main/java/edu/neu/cs5200/Cs5200Spring2018JaipuriaApplication.java
+++ src/main/java/edu/neu/cs5200/Cs5200Spring2018JaipuriaApplication.java
@@ -2,10 +2,12 @@ package edu.neu.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Spring2018JaipuriaApplication {
+public class Cs5200Spring2018JaipuriaApplication extends SpringBootServletInitializer {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Spring2018JaipuriaApplication.class, args);
 	}
